9e69317638c3a6976f04db1730caf37b0affc628
sophiexln <shinhy9@naver.com>
2025-06-15 15:09:11 +0900

    Add CaptureImageNode with ROS2 image subscription and image saving

--- include/runner/CaptureImageNode.hpp
+++ include/runner/CaptureImageNode.hpp
@@ -1,11 +1,30 @@
 #pragma once
-#include <behaviortree_cpp_v3/action_node.h>
 
-class CaptureImageNode : public BT::SyncActionNode
+#include "behaviortree_cpp_v3/action_node.h"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "runner1/srv/capture_image.hpp"
+
+namespace runner
+{
+
+class CaptureImageNode : public BT::AsyncActionNode
 {
 public:
   CaptureImageNode(const std::string& name, const BT::NodeConfiguration& config);
-  static BT::PortsList providedPorts();
+
+  static BT::PortsList providedPorts()
+  {
+    return { BT::InputPort<int>("capture_id") };
+  }
+
   BT::NodeStatus tick() override;
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+  sensor_msgs::msg::Image::SharedPtr latest_image_;
+  bool image_received_;
 };
 
+}  // namespace runner
--- src/CaptureImageNode.cpp
+++ src/CaptureImageNode.cpp
@@ -1,1 +1,67 @@
-#include <behaviortree_cpp_v3/bt_factory.h>
+#include "runner/CaptureImageNode.hpp"
+#include "behaviortree_cpp_v3/behavior_tree.h"
+
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <filesystem>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+namespace runner
+{
+
+CaptureImageNode::CaptureImageNode(const std::string& name, const BT::NodeConfiguration& config)
+: BT::AsyncActionNode(name, config)
+{
+  node_ = rclcpp::Node::make_shared("capture_image_node_bt");
+  image_received_ = false;
+
+  image_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
+    "/camera/image_raw", 10,
+    [this](sensor_msgs::msg::Image::SharedPtr msg) {
+      latest_image_ = msg;
+      image_received_ = true;
+    });
+}
+
+BT::NodeStatus CaptureImageNode::tick()
+{
+  int capture_id;
+  if (!getInput("capture_id", capture_id)) {
+    RCLCPP_ERROR(node_->get_logger(), "Missing input [capture_id]");
+    return BT::NodeStatus::FAILURE;
+  }
+
+  image_received_ = false;
+  rclcpp::Rate rate(10);
+  int tries = 30;
+  while (rclcpp::ok() && !image_received_ && tries-- > 0) {
+    rclcpp::spin_some(node_);
+    rate.sleep();
+  }
+
+  if (!image_received_ || !latest_image_) {
+    RCLCPP_ERROR(node_->get_logger(), "No image received");
+    return BT::NodeStatus::FAILURE;
+  }
+
+  cv_bridge::CvImagePtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(latest_image_, "bgr8");
+  } catch (cv_bridge::Exception& e) {
+    RCLCPP_ERROR(node_->get_logger(), "cv_bridge exception: %s", e.what());
+    return BT::NodeStatus::FAILURE;
+  }
+
+  std::string save_path = "/home/hayeon/runner1/logs/images/current/" + std::to_string(capture_id) + ".jpg";
+  if (!cv::imwrite(save_path, cv_ptr->image)) {
+    RCLCPP_ERROR(node_->get_logger(), "Failed to save image to %s", save_path.c_str());
+    return BT::NodeStatus::FAILURE;
+  }
+
+  RCLCPP_INFO(node_->get_logger(), "Image saved: %s", save_path.c_str());
+  return BT::NodeStatus::SUCCESS;
+}
+
+}  // namespace runner
